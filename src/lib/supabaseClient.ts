0e5c32f3ba96423abbf6a0b92dafb9b9c30c9575
rouchakova <rouchakova@sovrn.com>
2025-03-29 23:00:21 +0000

    Fix Supabase authentication

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://qpdeuqkzwkjglxumcnhe.supabase.co';
-const supabaseAnonKey = 'yeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwZGV1cWt6d2tqZ2x4dW1jbmhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MjQ0MjUsImV4cCI6MjA1NjAwMDQyNX0.CQpFSg-gqnPTAutjyZpaX0OJAESpuA-fJ_eAo0JuX2sour-anon-key';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwZGV1cWt6d2tqZ2x4dW1jbmhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MjQ0MjUsImV4cCI6MjA1NjAwMDQyNX0.CQpFSg-gqnPTAutjyZpaX0OJAESpuA-fJ_eAo0JuX2s';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
